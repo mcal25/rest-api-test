85b6db3c69d0f2209cc2efcdcfaeb05f724da8a0
mcal25 <225284010+mcal25@users.noreply.github.com>
2026-09-18 17:08:35 -0400

    Update RESTful methods

--- app.js
+++ app.js
@@ -7,9 +7,32 @@ const app = express();
 app.use(cors());
 
 
-app.get('/', (req, res) => {
-    res.send('Hi wurld');
-})
+
+app.get('/users', (req, res) => {
+  return res.send('GET HTTP method on user resource');
+});
+
+app.post('/users', (req, res) => {
+  return res.send('POST HTTP method on user resource');
+});
+
+app.put('/users/:userId', (req, res) => {
+  return res.send(
+    `PUT HTTP method on user/${req.params.userId} resource`,
+  );
+});
+
+app.delete('/users/:userId', (req, res) => {
+  return res.send(
+    `DELETE HTTP method on user/${req.params.userId} resource`,
+  );
+});
+
+
+
+
+
+
 
 
 app.listen(process.env.PORT, () => {
