650a40a4b544b53eb71701d84f74fa9c7e879763
mcal25 <225284010+mcal25@users.noreply.github.com>
2026-09-18 18:09:03 -0400

    Restructure folders to follow MVC

--- app.js
+++ app.js
@@ -1,73 +1,25 @@
-import 'dotenv/config';
-import cors from 'cors';
-import express from 'express';
+import "dotenv/config";
+import cors from "cors";
+import express from "express";
+import models from "./models/index.js";
+import routes from './routes/index.js';
 
 const app = express();
 
 app.use(cors());
-
-let users = {
-  1: {
-    id: '1',
-    username: 'Robin Wieruch',
-  },
-  2: {
-    id: '2',
-    username: 'Dave Davids',
-  },
-};
-
-let messages = {
-  1: {
-    id: '1',
-    text: 'Hello World',
-    userId: '1',
-  },
-  2: {
-    id: '2',
-    text: 'By World',
-    userId: '2',
-  },
-};
-
-app.get('/users', (req, res) => {
-  return res.send(Object.values(users));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use((req, res, next) => {
+  req.context = {
+    models,
+    me: models.users[1],
+  };
+  next();
 });
-
-app.get('/users/:userId', (req, res) => {
-  return res.send(users[req.params.userId]);
-});
-
-app.post('/users', (req, res) => {
-  return res.send('POST HTTP method on user resource');
-});
-
-app.put('/users/:userId', (req, res) => {
-  return res.send(
-    `PUT HTTP method on user/${req.params.userId} resource`,
-  );
-});
-
-app.delete('/users/:userId', (req, res) => {
-  return res.send(
-    `DELETE HTTP method on user/${req.params.userId} resource`,
-  );
-});
-
-
-app.get('/messages', (req, res) => {
-  return res.send(Object.values(messages));
-});
-
-app.get('/messages/:messageId', (req, res) => {
-  return res.send(messages[req.params.messageId]);
-});
-
-
-
-
-
+app.use('/session', routes.session);
+app.use('/users', routes.user);
+app.use('/messages', routes.message);
 
 app.listen(process.env.PORT, () => {
-    console.log(`running express on port ${process.env.PORT}`);
+  console.log(`running express on port ${process.env.PORT}`);
 });
