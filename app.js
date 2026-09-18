89f417d7ad13e854a82fad59bcad8006d7f4dc84
mcal25 <225284010+mcal25@users.noreply.github.com>
2026-09-18 17:17:45 -0400

    Flesh out RESTful routes

--- app.js
+++ app.js
@@ -6,10 +6,36 @@ const app = express();
 
 app.use(cors());
 
+let users = {
+  1: {
+    id: '1',
+    username: 'Robin Wieruch',
+  },
+  2: {
+    id: '2',
+    username: 'Dave Davids',
+  },
+};
 
+let messages = {
+  1: {
+    id: '1',
+    text: 'Hello World',
+    userId: '1',
+  },
+  2: {
+    id: '2',
+    text: 'By World',
+    userId: '2',
+  },
+};
 
 app.get('/users', (req, res) => {
-  return res.send('GET HTTP method on user resource');
+  return res.send(Object.values(users));
+});
+
+app.get('/users/:userId', (req, res) => {
+  return res.send(users[req.params.userId]);
 });
 
 app.post('/users', (req, res) => {
@@ -29,6 +55,13 @@ app.delete('/users/:userId', (req, res) => {
 });
 
 
+app.get('/messages', (req, res) => {
+  return res.send(Object.values(messages));
+});
+
+app.get('/messages/:messageId', (req, res) => {
+  return res.send(messages[req.params.messageId]);
+});
 
 
 
